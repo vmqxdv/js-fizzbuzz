b9badda4f3364edb39d20dfcb8d37fc02182ab2b
vmqxdv <vquintilio@gmail.com>
2025-02-25 17:13:24 +0100

    feat: Aggiunta funzioni per rimpiazzare numeri in parole

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-// Sto usando nodemon per vedere i risulatati
+// Sto usando nodemon per vedere i risultati
 
 try {
 
   for (let i = 0; i <= 100; i++) {
-    console.log(i);
+    let toStamp = i.toString();
+
+    if (toStamp.includes('3')) {
+      toStamp = toStamp.replace('3', 'Fizz');
+    }; 
+    
+    if (toStamp.includes('5')) {
+      toStamp = toStamp.replace('5', 'Buzz');
+    };
+
+    console.log(toStamp);
   };
 
 } catch(err) {
